810a225a6e5c4fb695685d6b9bc3bfc9e71c10ca
Zoe Nickson <zoe.nickson@sidingsmedia.com>
2025-10-10 14:49:55 +0100

    Initialises the repo (#1)
    * Initialises the repo
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>
    
    * Fixed spelling
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>
    
    ---------
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

---
+++ src/lib.cpp
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 The University of St Andrews
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "lib.hpp"
+
+#include <fmt/core.h>
+
+library::library() : name{fmt::format("{}", "citescoop-cli")} {}
---
+++ test/src/citescoop-cli_test.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 The University of St Andrews
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "lib.hpp"
+
+TEST_CASE("Name is citescoop-cli", "[library]") {
+  auto const lib = library{};
+  REQUIRE(lib.name == "citescoop-cli");
+}
