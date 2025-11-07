9a5ebbc3c5d30f5065021d1c1c6276e622f0682f
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-11-07 11:40:50 +0000

    Added cat command
    The cat command does exactly what you expect. It will read an input pbf file and output the DebugString representation for each message, along with the message's size in bytes.
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

--- src/main.cc
+++ src/main.cc
@@ -14,6 +14,7 @@
 
 #include "cli.h"
 #include "commands/base_command.h"
+#include "commands/cat_command.h"
 #include "commands/extract_command.h"
 #include "commands/help_command.h"
 
@@ -52,6 +53,10 @@ auto main(int argc, char** argv) -> int {
   cli.Register(command);
 
   command = std::shared_ptr<cli::BaseCommand>(new cli::HelpCommand(commands));
+  commands->push_back(command);
+  cli.Register(command);
+
+  command = std::shared_ptr<cli::BaseCommand>(new cli::CatCommand());
   commands->push_back(command);
   cli.Register(command);
 
