269ed46b75ce745c6719d10e14283c40ec28ed1c
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-11-08 22:45:07 +0000

    Added meta command
    The meta command displays some useful metadata about the generated pbf files.
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

--- src/main.cc
+++ src/main.cc
@@ -18,6 +18,7 @@
 #include "commands/cat_command.h"
 #include "commands/extract_command.h"
 #include "commands/help_command.h"
+#include "commands/meta_command.h"
 
 namespace cli = wikiopencite::citescoop::cli;
 
@@ -58,6 +59,10 @@ auto main(int argc, char** argv) -> int {
   cli.Register(command);
 
   command = std::shared_ptr<cli::BaseCommand>(new cli::CatCommand());
+  commands->push_back(command);
+  cli.Register(command);
+
+  command = std::shared_ptr<cli::BaseCommand>(new cli::MetaCommand());
   commands->push_back(command);
   cli.Register(command);
 
