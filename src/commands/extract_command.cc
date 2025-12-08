2825e69b0c437a9b5d329494e23243a86d9f5b13
Zoe Nickson <mjn6@st-andrews.ac.uk>
2025-12-08 13:44:46 +0000

    Added streaming based extraction
    This doesn't actually seem to make much difference on memory usage sadly, we should probably actually look at where the issue is rather than just guessing.
    
    Signed-off-by: Zoe Nickson <mjn6@st-andrews.ac.uk>

--- src/commands/extract_command.cc
+++ src/commands/extract_command.cc
@@ -5,18 +5,25 @@
 
 #include <arpa/inet.h>  // NOLINT(misc-include-cleaner)
 
+#include <cstdint>
 #include <cstdlib>
+#include <filesystem>  // NOLINT(build/c++17)
 #include <fstream>
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <stdexcept>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "boost/program_options/options_description.hpp"
 #include "boost/program_options/parsers.hpp"
 #include "boost/program_options/value_semantic.hpp"
 #include "boost/program_options/variables_map.hpp"
+#include "boost/uuid/random_generator.hpp"
+#include "boost/uuid/uuid.hpp"
+#include "boost/uuid/uuid_io.hpp"
 #include "citescoop/extract.h"
 #include "citescoop/io.h"
 #include "citescoop/parser.h"
@@ -29,9 +36,11 @@
 
 namespace wikiopencite::citescoop::cli {
 
-namespace options = boost::program_options;
 namespace cs = wikiopencite::citescoop;
+namespace fs = std::filesystem;
+namespace options = boost::program_options;
 namespace proto = wikiopencite::proto;
+namespace uuids = boost::uuids;
 
 ExtractCommand::ExtractCommand()
     // NOLINTNEXTLINE(whitespace/indent_namespace)
@@ -44,7 +53,12 @@ ExtractCommand::ExtractCommand()
     ("wiki", options::value<std::string>()->default_value("all"),
       "Name of wiki being processed. Used to set the language indicator"
       " of the file header.")
-    ("bz2", "The input is compressed using bzip2 compression.");
+    ("bz2", "The input is compressed using bzip2 compression.")
+    ("low-mem", "The reduced memory usage processing style should be used. "
+      "This involves writing the pages and revisions to temporary files "
+      "before recombining them at the end.")
+    ("tmp-dir", options::value<std::string>(), "Directory to store temporary "
+      "files when using the low memory mode.");
   // clang-format on
 }
 
@@ -54,45 +68,106 @@ int ExtractCommand::Run(
     // NOLINTNEXTLINE(whitespace/indent_namespace,misc-unused-parameters)
     struct GlobalOptions globals) {
   auto parsed_args = ParseArgs(args);
-  auto output_file = EnsureArgument<std::string>("output", parsed_args.first);
-  auto using_stdin = parsed_args.first.contains("stdin");
-  auto lang = WikipediaCodeToLanguage(
-      ExtractLangCode(EnsureArgument<std::string>("wiki", parsed_args.first)));
-  std::string input_file;
-  if (!using_stdin) {
-    input_file = EnsureArgument<std::string>("input", parsed_args.first);
-  }
-
   parser_ = std::make_shared<cs::Parser>(
       cs::ParserOptions{.ignore_invalid_ident = true});
+  SetExtractor(parsed_args.first);
 
-  if (parsed_args.first.contains("bz2")) {
-    spdlog::debug("Using bz2 extractor");
-    extractor_ = std::unique_ptr<cs::Extractor>(new cs::Bz2Extractor(parser_));
-  } else {
-    spdlog::debug("Using plain text extractor");
-    extractor_ = std::unique_ptr<cs::Extractor>(new cs::TextExtractor(parser_));
+  if (parsed_args.first.contains("low-mem")) {
+    return LowMemMode(parsed_args.first);
+  }
+
+  return NormalMode(parsed_args.first);
+}
+
+int ExtractCommand::NormalMode(const options::variables_map& args) {
+  spdlog::debug("Starting dump processing in normal mode");
+
+  auto output_file = EnsureArgument<std::string>("output", args);
+  auto using_stdin = args.contains("stdin");
+  auto lang = WikipediaCodeToLanguage(
+      ExtractLangCode(EnsureArgument<std::string>("wiki", args)));
+  std::string input_file;
+  if (!using_stdin) {
+    input_file = EnsureArgument<std::string>("input", args);
   }
 
   std::ofstream output(output_file,
                        std::ios::out | std::ios::binary | std::ios::trunc);
 
   if (using_stdin) {
-    ProcessFile(std::cin, &output, lang);
+    ProcessFileInMemory(std::cin, &output, lang);
   } else {
     std::ifstream input(input_file);
-    ProcessFile(input, &output, lang);
+    ProcessFileInMemory(input, &output, lang);
     input.close();
   }
 
   output.close();
+  return EXIT_SUCCESS;
+}
+
+int ExtractCommand::LowMemMode(const options::variables_map& args) {
+  spdlog::debug("Starting dump processing in low memory mode");
+
+  auto output_file = EnsureArgument<std::string>("output", args);
+  auto tmp_dir = fs::path(EnsureArgument<std::string>("tmp-dir", args));
+  auto using_stdin = args.contains("stdin");
+  auto lang = WikipediaCodeToLanguage(
+      ExtractLangCode(EnsureArgument<std::string>("wiki", args)));
+  std::string input_file;
+  if (!using_stdin) {
+    input_file = EnsureArgument<std::string>("input", args);
+  }
+
+  DirMustExist(tmp_dir);
+
+  auto run_uuid = GenerateUUID();
+  auto paths = GetPaths(tmp_dir, run_uuid);
+
+  std::ofstream pages_output(paths.pages, kWriteOpenMode);
+  std::ofstream revisions_output(paths.revisions, kWriteOpenMode);
+
+  std::pair<uint64_t, uint64_t> counts;
+
+  if (using_stdin) {
+    counts = extractor_->Extract(std::cin, &pages_output, &revisions_output);
+  } else {
+    std::ifstream input(input_file);
+    counts = extractor_->Extract(input, &pages_output, &revisions_output);
+    input.close();
+  }
+
+  auto fileheader = proto::FileHeader();
+  fileheader.set_page_count(counts.first);
+  fileheader.set_revision_count(counts.second);
+  fileheader.set_language(lang);
+
+  pages_output.close();
+  revisions_output.close();
+
+  std::ifstream pages_input(paths.pages, kReadOpenMode);
+  std::ifstream revisions_input(paths.revisions, kReadOpenMode);
+  std::ofstream output(output_file, kWriteOpenMode);
+  auto writer = cs::MessageWriter(&output);
+
+  spdlog::trace("Writing file header to output");
+  writer.WriteMessage(fileheader);
+
+  spdlog::trace("Copying pages and revisions");
+  output << revisions_input.rdbuf();
+  output << pages_input.rdbuf();
+
+  output.close();
+  pages_input.close();
+  revisions_input.close();
 
   return EXIT_SUCCESS;
 }
 
-void ExtractCommand::ProcessFile(std::istream& input, std::ostream* output,
-                                 // NOLINTNEXTLINE(whitespace/indent_namespace)
-                                 proto::Language lang) {
+void ExtractCommand::ProcessFileInMemory(
+    std::istream& input,     // NOLINT(whitespace/indent_namespace)
+    std::ostream* output,    // NOLINT(whitespace/indent_namespace)
+    proto::Language lang) {  // NOLINT(whitespace/indent_namespace)
   spdlog::trace("Starting extraction from input stream");
 
   auto [pages, revisions] = extractor_->Extract(input);
@@ -122,6 +197,25 @@ void ExtractCommand::ProcessFile(std::istream& input, std::ostream* output,
   }
 }
 
+void ExtractCommand::SetExtractor(const options::variables_map& args) {
+  if (args.contains("bz2")) {
+    spdlog::debug("Using bz2 extractor");
+    extractor_ = std::unique_ptr<cs::Extractor>(new cs::Bz2Extractor(parser_));
+  } else {
+    spdlog::debug("Using plain text extractor");
+    extractor_ = std::unique_ptr<cs::Extractor>(new cs::TextExtractor(parser_));
+  }
+}
+
+void ExtractCommand::DirMustExist(const std::filesystem::path& path) {
+  if (!std::filesystem::exists(path)) {
+    throw std::runtime_error("Directory does not exist: " + path.string());
+  }
+  if (!std::filesystem::is_directory(path)) {
+    throw std::runtime_error("Path is not a directory: " + path.string());
+  }
+}
+
 std::string ExtractCommand::ExtractLangCode(const std::string& input) {
   const std::string kSuffix = "wiki";
   auto pos = input.rfind(kSuffix);
@@ -131,5 +225,24 @@ std::string ExtractCommand::ExtractLangCode(const std::string& input) {
   }
 
   return input;
+}
+
+ExtractCommand::TempPaths ExtractCommand::GetPaths(
+    const fs::path& temp_dir,  // NOLINT(whitespace/indent_namespace)
+    const std::string& uuid    // NOLINT(whitespace/indent_namespace)
+) {
+  auto paths = TempPaths{.pages = temp_dir / (uuid + "-pages.tmp"),
+                         .revisions = temp_dir / (uuid + "-revisions.tmp")};
+
+  spdlog::debug(
+      "Using following temporary paths for messages: pages: {}, revisions: {}",
+      paths.pages.string(), paths.revisions.string());
+  return paths;
+}
+
+std::string ExtractCommand::GenerateUUID() {
+  // NOLINTNEXTLINE(readability-identifier-naming)
+  const uuids::uuid uuid = uuids::random_generator()();
+  return uuids::to_string(uuid);
 }
 }  // namespace wikiopencite::citescoop::cli
--- src/commands/extract_command.h
+++ src/commands/extract_command.h
@@ -4,14 +4,15 @@
 #ifndef SRC_COMMANDS_EXTRACT_COMMAND_H_
 #define SRC_COMMANDS_EXTRACT_COMMAND_H_
 
-// NOLINTNEXTLINE(build/c++17)
-#include <filesystem>
+#include <filesystem>  // NOLINT(build/c++17)
+#include <ios>
 #include <istream>
 #include <memory>
 #include <ostream>
 #include <string>
 #include <vector>
 
+#include "boost/program_options/variables_map.hpp"
 #include "citescoop/extract.h"
 #include "citescoop/parser.h"
 #include "citescoop/proto/language.pb.h"
@@ -26,15 +27,36 @@ class ExtractCommand : public BaseCommand {
   int Run(std::vector<std::string> args, GlobalOptions globals) override;
 
  private:
-  std::filesystem::path dump_root_;
-  std::filesystem::path output_dir_;
+  struct TempPaths {
+    std::filesystem::path pages;
+    std::filesystem::path revisions;
+  };
+
   std::shared_ptr<wikiopencite::citescoop::Parser> parser_;
+
   std::unique_ptr<wikiopencite::citescoop::Extractor> extractor_;
 
-  void ProcessFile(std::istream& input, std::ostream* output,
-                   wikiopencite::proto::Language lang);
+  static const std::ios_base::openmode kWriteOpenMode =
+      std::ios::out | std::ios::binary | std::ios::trunc;
+  static const std::ios_base::openmode kReadOpenMode =
+      std::ios::in | std::ios::binary;
+
+  int NormalMode(const boost::program_options::variables_map& args);
+  int LowMemMode(const boost::program_options::variables_map& args);
+
+  void ProcessFileInMemory(std::istream& input, std::ostream* output,
+                           wikiopencite::proto::Language lang);
+
+  void SetExtractor(const boost::program_options::variables_map& args);
+
+  static void DirMustExist(const std::filesystem::path& path);
 
   static std::string ExtractLangCode(const std::string& input);
+
+  static std::string GenerateUUID();
+
+  static TempPaths GetPaths(const std::filesystem::path& temp_dir,
+                            const std::string& uuid);
 };
 
 }  // namespace wikiopencite::citescoop::cli
